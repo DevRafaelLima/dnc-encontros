818783db5d7d7d58e9020b7b3be3150f8c901a77
Rafael S. de Lima <rafael.lima@startse.com>
2022-12-18 10:00:55 -0300

    conclusão do desáfio básico do dia 4

--- day-04/dasafio-basico/assets/js/script.js
+++ day-04/dasafio-basico/assets/js/script.js
@@ -2,21 +2,82 @@ let medicamentos = []
 
 function addMedicamentos() {
     event.preventDefault();
+    let nome = document.getElementById('fnomeMedicamento').value;
+    let qtdDisponivel = document.getElementById('fqtdDisponivelMedicamento').value;
+    let precoUnidade = document.getElementById('fprecoUnidaddeMedicamento').value;
+    let classeMedicamento = document.getElementById('fclasseMedicamento').value;
+
     let novoMedicamento = {
         "id": medicamentos.length + 1,
-        "nome": document.getElementById('fnomeMedicamento').value,
-        "qtdDisponivel": document.getElementById('fqtdDisponivelMedicamento').value,
-        "precoUnidade": document.getElementById('fprecoUnidaddeMedicamento').value,
-        "classeMedicamento": document.getElementById('fclasseMedicamento').value,
+        "nome": nome,
+        "qtdDisponivel": qtdDisponivel,
+        "precoUnidade": precoUnidade,
+        "classeMedicamento": classeMedicamento,
 
     }
-    medicamentos.push(novoMedicamento)
-    clearInputs()
+
+    if (nome === '' || qtdDisponivel === '' || precoUnidade === '' || classeMedicamento === '') {
+        renderAlert();
+    } else  {
+        medicamentos.push(novoMedicamento)
+        renderPaginaDetalhe(nome, qtdDisponivel, precoUnidade)
+    }
+
+
+}
+function renderPaginaDetalhe(nome, qtdDisponivel, precoUnidade) {
+    let formSection = document.getElementById('formSection')
+    formSection.innerHTML = `<div class="detalhesMedicamento">
+    <h1>Novo Medicamento adicionado!</h1>
+    <div class="informacoes">
+        <h3>${nome}</h3>
+        <h3>${qtdDisponivel} unidades do disponíveis</h3>
+        <h3>${precoUnidade} reais por unidade</h3>
+    </div>
+    <div class="areaBotao">
+        <button onclick="renderAddMedicamento()">Voltar</button>
+    </div>
+</div>`
 }
 
-function clearInputs() {
-    document.getElementById('fnomeMedicamento').value = ''
-    document.getElementById('fqtdDisponivelMedicamento').value = ''
-    document.getElementById('fprecoUnidaddeMedicamento').value = ''
-    document.getElementById('fclasseMedicamento').value = ''
+function renderAddMedicamento() {
+    let formSection = document.getElementById('formSection')
+    formSection.innerHTML =`
+<form onsubmit="addMedicamentos()">
+    <p>
+        <label for="fnomeMedicamento">Nome do medicamento</label><br>
+        <input type="text" id="fnomeMedicamento" name="nomeMedicamento">
+    </p>
+    <p>
+        <label for="fqtdDisponivelMedicamento">Quantidade Disponível</label><br>
+        <input type="text" id="fqtdDisponivelMedicamento">
+    </p>
+    <p>
+        <label for="fprecoUnidaddeMedicamento">Preço por Unidade</label><br>
+        <input type="text" id="fprecoUnidaddeMedicamento">
+    </p>
+    <p>
+        <label for="fclasseMedicamento">Classe de Medicamento</label><br>
+        <input type="text" id="fclasseMedicamento">
+    </p>
+
+    <div class="areaBotao">
+        <button type="submit">Cadastrar</button>
+    </div>
+</form>`
+}
+
+function renderAlert() {
+    let formSection = document.getElementById('formSection')
+    formSection.innerHTML =`
+        <div class="areaAlert">
+            <div>
+                <h2>Desculpe, não foi possível concluir o cadastro</h2>
+                <p>Você precisa preencher todos os campos para cadastrar o medicamento</p>
+            </div>
+            <div class="areaBotao">
+                <button class="buttonAlert" onclick="renderAddMedicamento()">OK</button>
+            </div>
+        </div>
+    `
 }
